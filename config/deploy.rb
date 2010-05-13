185cecdda0c1a13bffe3c069594018ce21c8204d
Rick Harris <rconradharris@gmail.com>
2010-05-13 00:34:11 -0500

    Adding thin.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,3 +13,19 @@ set :user, "deploy"
 
 server "exmachina.rconradharris.com", :app, :web, :db, :primary => true
 
+namespace :deploy do
+  %w(start stop restart).each do |action|
+     desc "#{action} the Thin processes"
+     task action.to_sym do
+       find_and_execute_task("thin:#{action}")
+    end
+  end
+end
+namespace :thin do
+  %w(start stop restart).each do |action|
+  desc "#{action} the app's Thin Cluster"
+    task action.to_sym, :roles => :app do
+      run "thin #{action} -c #{deploy_to}/current -C #{deploy_to}/current/config/thin.yml"
+    end
+  end
+end
